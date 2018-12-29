ea333afc6504f0f1c1752df8ce437b0058bebf29
Rolf Kristensen <sweaty1@hotmail.com>
2018-12-29 23:05:56 +0100

    VS2017 Conversion and added NetStandard2.0 support

--- NLog.Slack/Properties/AssemblyInfo.cs
+++ NLog.Slack/Properties/AssemblyInfo.cs
@@ -2,18 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("NLog.Slack")]
-[assembly: AssemblyDescription("An NLog target for Slack - your logs in one place and instantly searchable, everywhere.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ifc0nfig")]
-[assembly: AssemblyProduct("NLog.Slack")]
-[assembly: AssemblyCopyright("Copyright © Paul Price 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -22,15 +10,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c64063cf-1bab-4bbf-aa88-587eac04ed33")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
